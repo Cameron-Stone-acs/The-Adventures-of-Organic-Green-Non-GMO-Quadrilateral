533bb26bb17ce6e09130a186c7aff5b1088db2a0
K1d_Named_F1nger <143749881+Fireice2@users.noreply.github.com>
2026-09-04 09:25:08 -0700

    Update CheckPoint.cs

--- Assets/Scripts/Checkpoints/CheckPoint.cs
+++ Assets/Scripts/Checkpoints/CheckPoint.cs
@@ -2,16 +2,5 @@ using UnityEngine;
 
 public class CheckPoint : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     public float checkpointNumber;
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
